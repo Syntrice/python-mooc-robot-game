71e25e05fa3274e2ea1f45a01e078788a1fbe0a8
syntrice <76839403+Syntrice@users.noreply.github.com>
2024-09-27 14:33:14 +0100

    Add tile getter and setters for world class
    The world class now has methods for retrieving and setting a tile
    type at a given coordinate.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pygame
 from random import random
 from enum import Enum
+from __future__ import annotations
 
 GAME_TITLE = "Robot Game"
 WINDOW_WIDTH = 640
@@ -34,10 +35,11 @@ class GameApplication:
             self.render()
             self.clock.tick(FPS)
 
+
 class World:
     """
     A class representing a tile-based game world, on which to keep track of
-    tiles to render and update. Tile types are currently defined using integer 
+    tiles to render and update. Tile types are currently defined using integer
     IDs.
     """
 
@@ -46,24 +48,32 @@ class World:
         self._height = height
         self._tile_grid = self._generate_random_tiles()
 
-        
     def _generate_random_tiles(self) -> None:
         tile_grid = []
         for i in range(self._width):
             tile_grid.append([])
             for j in range(self._height):
-                tile_grid[i].append(Tile.WALL.value if random() < 0.5 else Tile.FLOOR.value)
+                tile_grid[i].append(
+                    Tile.WALL.value if random() < 0.5 else Tile.FLOOR.value
+                )
         return tile_grid
+
+    def get_tile_at_position(self, x: int, y: int) -> Tile:
+        return Tile(self._tile_grid[x][y])
+
+    def set_tile_at_position(self, x: int, y: int, tile: Tile) -> None:
+        self._tile_grid[x][y] = tile.value
 
     @property
     def width(self):
         """The width property."""
         return self._width
-        
+
     @property
     def height(self):
         """The height property."""
         return self._height
+
 
 class Tile(Enum):
     """
@@ -71,16 +81,17 @@ class Tile(Enum):
     present in the game world. Each tile type has an ID, a color, and a flag
     indicating whether the tile is collidable or not.
     """
-    
+
     FLOOR = (0, (120, 120, 120), False)
     WALL = (1, (160, 160, 160), True)
-    
+
     def __new__(cls, tile_id, color, is_collidable):
         obj = object.__new__(cls)
         obj._value_ = tile_id
         obj.color = color
         obj.is_collidable = is_collidable
         return obj
+
 
 if __name__ == "__main__":
     game = GameApplication()
