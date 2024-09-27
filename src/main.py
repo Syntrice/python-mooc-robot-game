8da2a1f84f4a72e97e135dd5d4e9a19040903325
syntrice <76839403+Syntrice@users.noreply.github.com>
2024-09-27 13:38:37 +0100

    Add PyGame boilerplate code and setup for game
    Add boilerplate code to display a window, along with an outline for
    render and update methods and game loop, contained within a main game
    class. By encapsulating the code in an object oriented setting,
    it will allow for cleaner organization, along with ease of addition of
    new features.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,35 @@
-# Complete your game here
+import pygame
+
+GAME_TITLE = "Robot Game" 
+WINDOW_WIDTH = 640
+WINDOW_HEIGHT = 480
+FPS = 60
+
+class GameApplication:
+    def __init__(self) -> None:
+        pygame.init()
+        self.window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption(GAME_TITLE)
+        self.clock = pygame.time.Clock()
+        self.run()
+        
+    def update(self) -> None:
+        self.handle_events()
+    
+    def render(self) -> None:
+        pass
+    
+    def handle_events(self) -> None:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+    
+    def run(self) -> None:
+        while True:
+            self.update()
+            self.render()
+            self.clock.tick(FPS)
+        
+if __name__ == "__main__":
+    game = GameApplication()
